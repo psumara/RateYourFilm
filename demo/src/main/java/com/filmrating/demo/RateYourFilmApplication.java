b13650c7365c2c6e96e67305050c699e00a34268
p.sumara <sumara0498@icloud.com>
2020-10-10 19:30:45 +0200

    Add spring boot project

---
+++ demo/src/main/java/com/filmrating/demo/RateYourFilmApplication.java
@@ -0,0 +1,13 @@
+package com.filmrating.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateYourFilmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RateYourFilmApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/filmrating/demo/RateYourFilmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filmrating.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RateYourFilmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
